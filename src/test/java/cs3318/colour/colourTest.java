f7d042223f7305fa56be58d2063e16151186619e
AlexHoangM <72013537+AlexHoangM@users.noreply.github.com>
2022-11-29 23:58:15 +0000

    create parameterized tests for float and bit type

--- src/test/java/cs3318/colour/colourTest.java
+++ src/test/java/cs3318/colour/colourTest.java
@@ -1,6 +1,11 @@
 package cs3318.colour;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -60,5 +65,37 @@ public class colourTest {
         int element = 1791;
 
         assertDoesNotThrow(() -> new Colour(element));
+    }
+
+    static Stream<Arguments> testCasesFloat() {
+        return Stream.of(
+                Arguments.of(0.1f, 0.2f, 0.3f),
+                Arguments.of(-0.1f, 0.2f, 0.3f),
+                Arguments.of(0.3f, 0.4f, 1.9f)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("testCasesFloat")
+    public void exceptionIfElementOutOfRange(float element1, float element2, float element3) {
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> new Colour(element1, element2, element3));
+
+        assertEquals("Color element should be in range of float 0.0 and 1.0", exception.getMessage());
+    }
+
+
+    static Stream<Arguments> testCasesBit() {
+        return Stream.of(
+                Arguments.of(23157),
+                Arguments.of(-1157),
+                Arguments.of(23167)
+        );
+    }
+    @ParameterizedTest
+    @MethodSource("testCasesBit")
+    public void exceptionIfElementOutOfRange(int element) {
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> new Colour(element));
+
+        assertEquals("Color element should be in range 16-23 for red; 8-15 for green; 0-7 for blue", exception.getMessage());
     }
 }
