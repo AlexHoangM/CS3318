6bf85c13a619e66b9b5b7c294aea2babea8c3ad2
AlexHoangM <72013537+AlexHoangM@users.noreply.github.com>
2022-11-29 12:07:53 +0000

    change maven compiler to ver 11 then create and run default tests for float and bit inputs

--- src/main/java/cs3318/colour/Colour.java
+++ src/main/java/cs3318/colour/Colour.java
@@ -1,4 +1,29 @@
 package cs3318.colour;
 
 public class Colour {
+
+    private float[] valuesFloat;
+    private int[] valuesBit;
+
+    private final static float minRange = 0.0F;
+    private final static float maxRange = 1.0F;
+
+    public Colour(float element1, float element2, float element3) {
+        valuesFloat = new float[] {element1, element2, element3};
+
+    }
+
+    public Colour(int element1, int element2, int element3) {
+        valuesBit = new int[] {element1, element2, element3};
+
+    }
+
+
+    public float[] getValuesFloat() {
+        return valuesFloat;
+    }
+
+    public int[] getValuesBit() {
+        return valuesBit;
+    }
 }
--- src/test/java/cs3318/colour/colourTest.java
+++ src/test/java/cs3318/colour/colourTest.java
@@ -1,4 +1,30 @@
 package cs3318.colour;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class colourTest {
+
+    @Test
+    public void checkColorValuesCorrectDefaultRGBFloat() {
+        float element1 = 0.1F;
+        float element2 = 0.3F;
+        float element3 = 0.5F;
+
+        Colour color = new Colour(element1, element2, element3);
+
+        assertArrayEquals(color.getValuesFloat(), color.getValuesFloat());
+    }
+
+    @Test
+    public void checkColorValuesCorrectDefaultRGBBit() {
+        int element1 = 17;
+        int element2 = 9;
+        int element3 = 1;
+
+        Colour color = new Colour(element1, element2, element3);
+
+        assertArrayEquals(color.getValuesBit(), color.getValuesBit());
+    }
 }
